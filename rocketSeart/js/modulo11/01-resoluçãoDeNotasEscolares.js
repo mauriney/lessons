82e9e6f1817c500ac5347738bbd9ce3ee698b8c5
Mauriney <maurineyy@gmail.com>
2021-10-13 09:10:36 -0500

    js rocketseat exercise resolution

---
+++ rocketSeart/js/modulo11/01-resoluçãoDeNotasEscolares.js
@@ -0,0 +1,27 @@
+/*
+Crie um algoritmo que transforme as notas do sistema nmérico para sistema de notas em caracteres tipo A B Crie
+ # de 90 para cima  - A 
+ # entre 80 - 90    - B
+ # entre 70 - 79    - C
+ # entre 60 - 69    - D
+ # menor que 60     - F
+ */
+const nota = 60
+
+if (nota < 60){
+  console.log ('Nota F')
+} else{
+  if (nota <= 69){
+    console.log('Nota D')
+  }else{
+    if (nota <= 79){
+      console.log('Nota C')
+    } else{
+      if (nota <= 89){
+        console.log('Nota B')
+      } else {
+        console.log('Nota A')
+      }
+    }
+  }
+}
