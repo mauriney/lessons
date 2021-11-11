d344a0f55600ff4d9b7e4bf43190e9416439c599
Mauriney <maurineyy@gmail.com>
2021-11-11 09:46:16 -0500

    finished digitalInovation

--- digitalInovation/node-express/index.js
+++ digitalInovation/node-express/index.js
@@ -5,7 +5,7 @@ const userRoute = require('./routes/userRoute')
 const app = express()
 const port = 3000
 
-app.use(bodyParser.urlencoded({ extended: false}))
+app.use('bodyParser'.urlencoded({ extended: false}))
 
 
 userRoute(app)
